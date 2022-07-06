3af5e5603c0435d9dc85c2388aeb7c057735c9f6
Min Seok Song <95131573+LearnerPS@users.noreply.github.com>
2022-07-06 22:08:46 +0900

    Create Calc.java

---
+++ Calc.java
@@ -0,0 +1,19 @@
+***
+//내가 포기 할 때까진 아직 끝난 게 아니다
+***
+```ruby
+
+package spring_01;
+
+import org.springframework.stereotype.Component;
+
+@Component("calc")
+public class Calc {
+
+	public int plus(int i, int j) {
+		return i + j;
+	}
+
+}
+
+```
