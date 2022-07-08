1208bc6cc69f623c3c07881761ed707481235ad5
Min Seok Song <95131573+LearnerPS@users.noreply.github.com>
2022-07-08 19:06:13 +0900

    Create Hello2.java

---
+++ Hello2.java
@@ -0,0 +1,15 @@
+***
+//내가 포기 할 때까진 아직 끝난 게 아니다
+***
+```ruby
+
+package spring_01;
+
+public class Hello2 extends Hello {
+	@Override
+	public void sayHello() {
+		System.out.println("안녕");
+	}
+}
+
+```
