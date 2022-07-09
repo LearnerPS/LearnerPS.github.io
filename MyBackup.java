7c9c9c5d9112287227496872b93fe3a996024a86
Min Seok Song <95131573+LearnerPS@users.noreply.github.com>
2022-07-09 20:52:27 +0900

    Create MyBackup.java

---
+++ MyBackup.java
@@ -0,0 +1,19 @@
+***
+//내가 포기 할 때까진 아직 끝난 게 아니다
+***
+```ruby
+
+package spring_01;
+
+import org.springframework.stereotype.Component;
+
+
+public class MyBackup {
+
+	public void log(String string) {
+		System.out.println("backup: ȭ�� ���: "+string);
+	}
+
+}
+
+```
