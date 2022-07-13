054c155b1829f5313346ba3827b358308d1d7889
Min Seok Song <95131573+LearnerPS@users.noreply.github.com>
2022-07-13 22:09:48 +0900

    Create TestHello_Annotation.java

---
+++ TestHello_Annotation.java
@@ -0,0 +1,39 @@
+***
+//내가 포기 할 때까진 아직 끝난 게 아니다
+***
+```ruby
+
+package spring_01;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
+import org.springframework.context.support.GenericXmlApplicationContext;
+
+public class TestHello_Annotation {
+
+	public static void main(String[] args) {
+		//������ �����̳� �θ���
+		BeanFactory context = 
+//				new FileSystemXmlApplicationContext("applicationContext.xml");
+//				new ClassPathXmlApplicationContext("/config/spring.xml");
+				new GenericXmlApplicationContext("classpath:/config/spring_annotation.xml");
+		Hello h1 = (Hello)context.getBean("hello");
+		h1.sayHello();
+///		
+		Calc c1 = (Calc)context.getBean("calc");
+		int i = c1.plus( 2,3 );
+		System.out.println( i ); // 5
+///		
+		MySum ms = (MySum)context.getBean("mysum");
+		ms.add( 1 );
+		ms.add( 2 );
+		int t = ms.getTotal();
+		System.out.println(t); // => 3
+		
+	}
+
+}
+
+
+```
