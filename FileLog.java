924cf39f064186dc4056aa184c2383adfbb2c340
Min Seok Song <95131573+LearnerPS@users.noreply.github.com>
2022-07-16 19:57:23 +0900

    Create FileLog.java

---
+++ FileLog.java
@@ -0,0 +1,18 @@
+***
+//내가 포기 할 때까진 아직 끝난 게 아니다
+***
+```ruby
+
+package spring_02;
+
+public class FileLog extends MyLog {
+	
+	@Override
+	public void printLog(String string) {
+		System.out.println("FileLog: "+string);
+	}
+	
+}
+
+
+```
