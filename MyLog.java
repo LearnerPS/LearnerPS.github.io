75fb80a96bb5f4dd4db566436dd11f0b9d0d93f7
Min Seok Song <95131573+LearnerPS@users.noreply.github.com>
2022-07-18 20:08:32 +0900

    Create MyLog.java

---
+++ MyLog.java
@@ -0,0 +1,16 @@
+***
+//내가 포기 할 때까진 아직 끝난 게 아니다
+***
+```ruby
+
+package spring_02;
+
+public class MyLog {
+
+	public void printLog(String string) {
+		System.out.println("MyLog: "+string);
+	}
+
+}
+
+```
