f81000dee16c5bb2f3ddfc7e8ad9c6044d5959dd
Min Seok Song <95131573+LearnerPS@users.noreply.github.com>
2022-08-11 21:44:34 +0900

    Create TodoNotFoundException.java

---
+++ TodoNotFoundException.java
@@ -0,0 +1,17 @@
+***
+//내가 포기 할 때까진 아직 끝난 게 아니다
+***
+```ruby
+
+package org.sesac.todo_04.exception;
+
+public class TodoNotFoundException extends Exception {
+
+	public TodoNotFoundException(String string) {
+		super(string);
+	}
+
+}
+
+
+```
